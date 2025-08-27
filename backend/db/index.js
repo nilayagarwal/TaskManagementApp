b3b0e1c497c5a29d69de4af1d96d25f0c572ec79
nilayagarwal <71015004+nilayagarwal@users.noreply.github.com>
2025-08-27 20:26:51 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-// Connect to MongoDB
+
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
